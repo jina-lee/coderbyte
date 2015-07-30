6c121330828f7b67ec452016ba5b6e6bf676f0c8
jina-lee <dkrak5105@naver.com>
2015-07-30 21:26:50 +0900

    Create Longest Word.rb

---
+++ Longest Word.rb
@@ -0,0 +1,15 @@
+def LongestWord(sen)
+
+  # code goes here
+ 
+  
+  
+arr = sen.split(" ")
+  arr.sort! { |a, b| b.length <=> a.length }
+  arr[0]
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LongestWord(STDIN.gets)    
