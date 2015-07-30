ace0768a5b5632f820fea32058920948e87502fd
jina-lee <dkrak5105@naver.com>
2015-07-30 21:31:53 +0900

    Create Palindrome.rb

---
+++ Palindrome.rb
@@ -0,0 +1,15 @@
+
+def Palindrome(str)
+
+  # code goes here
+  if str == str.reverse
+    true
+  else
+    return false
+  end
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+print Palindrome(STDIN.gets) 
