9914d1c7a13c54fdc88de64b98f4363b304f4b57
jina-lee <dkrak5105@naver.com>
2015-07-30 21:25:14 +0900

    Create SimpleAdding.rb

---
+++ SimpleAdding.rb
@@ -0,0 +1,11 @@
+def SimpleAdding(num)
+
+  # code goes here
+  range_sum = *(1..num)
+  return range_sum.inject(:+)
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+print SimpleAdding(STDIN.gets)           
